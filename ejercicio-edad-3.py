c6371911bc8e47cfde85326014990eb65ba49741
bryhan360 <blaciar.lonsallez@gmail.com>
2017-03-07 01:58:43 +0100

    Add files via upload

---
+++ ejercicio-edad-3.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+edad = input("Introduzca su edad: ")
+if ( edad >= 18 and edad <= 23 or edad ==17) :
+	print "You can enter in the youngster season"
+else :
+	print "Sorry mate, you can't enter"
